6814373ae27e53bf9406bcab1d832727957b000c
Jørgen Christian Heinsen <c.heinsen@gmail.com>
2017-05-24 14:48:28 +0200

    Made helper method for the availability test run. Added availability item template.

--- models/availability.js
+++ models/availability.js
@@ -1,17 +1,47 @@
 var express = require('express');
 var benchrest = require('bench-rest');
 var moment = require('moment');
+const jade = require('jade');
+var fs = require('fs');
+
+const availabilityTestCompiledFunction = jade.compileFile('views/partials/availability-item-template.jade');
 
 var exec = require('child_process').exec;
 var child;
+
+function availabilityTestRun(host, nNodes, nRequests, startRate, nSteps) {
+  const startDate = moment().format();
+  const resultsFileName = 'availability_test_result/availability-test-' + startDate + '-nodes-' + nNodes + '-nRequests-' + nRequests + '-nSteps-' + nSteps;
+
+  fs.writeFile(resultsFileName, '', function(err) {
+    if(err) {
+        return console.log(err);
+    }
+
+    console.log("The file was saved!");
+  });
+
+  recursiveTestRun(host, nNodes, nRequests, startRate, 1, nSteps, resultsFileName);
+}
+
+function recursiveTestRun(host, nNodes, nRequests, rate, step, nSteps, resultsFileName) {
+  console.log('host:' + host + ' nNodes:' + nNodes + ' nRequests:' + nRequests + ' Rate:' + rate + ' Step:' + step + 'nSteps:' + nSteps);
+  availabilityTest(host, nNodes, nRequests, rate, function(fileName) {
+    availabilityReadFile(nNodes, fileName, resultsFileName, function() {
+      if(step <= nSteps) {
+        recursiveTestRun(host, nNodes, nRequests, rate+100, step+1, nSteps, resultsFileName);
+      }
+    });
+  });
+}
 
 function availabilityTest(host, nNodes, nRequests, rate, callback) {
   const date = moment().format();
   const duration = ((nRequests + (rate-1))/rate);
 
-  const fileName = 'availability_tests/availability-' + date + '-nodes-' + nNodes + '-nRequests-' + nRequests + '-rate-' + rate + '-duration-' + duration;
+  const fileName = 'availability_test/availability-' + date + '-nodes-' + nNodes + '-nRequests-' + nRequests + '-rate-' + rate + '-duration-' + duration;
   console.log('duration: ' + duration);
-  const command = 'artillery quick --duration '+ duration +' --rate '+ rate +' -n 1 --output ' + fileName + ' '+ host;
+  const command = 'artillery quick --duration '+ duration +' --rate '+ rate +' -n 1 --output ' + fileName + ' ' + host;
 
   child = exec(command, function (error, stdout, stderr) {
     console.log('stdout: ' + stdout);
@@ -23,9 +53,7 @@ function availabilityTest(host, nNodes, nRequests, rate, callback) {
   });
 }
 
-var fs = require('fs');
-
-function availabilityReadFile(fileName) {
+function availabilityReadFile(nNodes, fileName, resultsFileName, callback) {
   fs.readFile(fileName, 'utf8', function (err, data) {
     if (err) throw err; // we'll not consider error handling for now
     var obj = JSON.parse(data);
@@ -36,12 +64,24 @@ function availabilityReadFile(fileName) {
     console.log("Arrival Rate:");
     console.log(obj.aggregate.phases[0].arrivalRate);
 
+    const testResultItem = availabilityTestCompiledFunction({
+        nodes: nNodes,
+        rate: obj.aggregate.phases[0].arrivalRate,
+        duration: obj.aggregate.phases[0].duration,
+        median: obj.aggregate.scenarioDuration.median
+    })
 
-
+    fs.appendFile(resultsFileName, testResultItem, function (err) {
+      if (err) throw err;
+      console.log('Saved!');
+    });
   });
+
+  callback();
 }
 
 module.exports = {
+ availabilityTestRun: availabilityTestRun,
  availabilityTest : availabilityTest,
  availabilityReadFile: availabilityReadFile
 }
--- routes/index.js
+++ routes/index.js
@@ -6,21 +6,12 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   const host = 'http://localhost:8080/rules';
-  
   const nRequests = 10000;
-  const stepSize = 1000;
+  const nNodes = 1;
+  const startRate = 1000;
+  const nSteps = 1;
 
-  // model.availability(host, nRequests, stepSize, function() {
-    
-  // });
-
-  // model.availabilitySecond(host, nRequests, stepSize, function() {
-
-
-  // });
-  model.availabilityTest(host, 1, nRequests, stepSize, function(fileName) {
-    model.availabilityReadFile(fileName);
-  });
+  model.availabilityTestRun(host, nNodes, nRequests, startRate, nSteps);
 
   res.render('index', { title: 'Availability test' });
 });
