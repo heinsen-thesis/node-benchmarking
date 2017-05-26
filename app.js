a3fc84348462c5decf054f7e5b33c556b7f4f772
Jørgen Christian Heinsen <c.heinsen@gmail.com>
2017-05-26 12:36:41 +0200

    Added fork of server, hoping for it to solve concurrency problems with test.

--- app.js
+++ app.js
@@ -4,61 +4,71 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cluster = require('cluster');  
+var numCPUs = require('os').cpus().length;
 
 var routes = require('./routes/index');
 
-var app = express();
+if (cluster.isMaster) {  
+    for (var i = 0; i < numCPUs; i++) {
+        // Create a worker
+        cluster.fork();
+    }
+} else {
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+  var app = express();
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/bootstrap', express.static(path.join(__dirname,'./node_modules/bootstrap/dist/')));
-app.use('/jquery', express.static(path.join(__dirname,'./node_modules/jquery/dist/')));
-app.use('/ajax', express.static(path.join(__dirname,'./node_modules/ajax/lib/')));
+  // view engine setup
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'jade');
 
-app.use('/', routes);
+  // uncomment after placing your favicon in /public
+  //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+  app.use(logger('dev'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.use('/bootstrap', express.static(path.join(__dirname,'./node_modules/bootstrap/dist/')));
+  app.use('/jquery', express.static(path.join(__dirname,'./node_modules/jquery/dist/')));
+  app.use('/ajax', express.static(path.join(__dirname,'./node_modules/ajax/lib/')));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+  app.use('/', routes);
 
-// error handlers
+  // catch 404 and forward to error handler
+  app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  });
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
+  // error handlers
+
+  // development error handler
+  // will print stacktrace
+  if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+      res.status(err.status || 500);
+      res.render('error', {
+        message: err.message,
+        error: err
+      });
+    });
+  }
+
+  // production error handler
+  // no stacktraces leaked to user
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
-      error: err
+      error: {}
     });
   });
+
+  app.listen(3000, function () {
+    console.log('Example app listening on port 8080!')
+  })
+
+  module.exports = app;  
 }
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
-})
-
-module.exports = app;
