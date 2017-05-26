afdf68e34584dc2ec1ca8dbdc68e65e6185dd7d9
Jørgen Christian Heinsen <c.heinsen@gmail.com>
2017-05-26 13:45:49 +0200

    Redid forking logic.

--- app.js
+++ app.js
@@ -5,70 +5,74 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var cluster = require('cluster');  
-var numCPUs = require('os').cpus().length;
 
 var routes = require('./routes/index');
 
+var app = express();
+
+
 if (cluster.isMaster) {  
-    for (var i = 0; i < numCPUs; i++) {
-        // Create a worker
-        cluster.fork();
-    }
-} else {
+    const worker = cluster.fork();
+    console.log('Forking');
+}
 
-  var app = express();
 
-  // view engine setup
-  app.set('views', path.join(__dirname, 'views'));
-  app.set('view engine', 'jade');
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-  // uncomment after placing your favicon in /public
-  //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-  app.use(logger('dev'));
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: false }));
-  app.use(cookieParser());
-  app.use(express.static(path.join(__dirname, 'public')));
-  app.use('/bootstrap', express.static(path.join(__dirname,'./node_modules/bootstrap/dist/')));
-  app.use('/jquery', express.static(path.join(__dirname,'./node_modules/jquery/dist/')));
-  app.use('/ajax', express.static(path.join(__dirname,'./node_modules/ajax/lib/')));
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/bootstrap', express.static(path.join(__dirname,'./node_modules/bootstrap/dist/')));
+app.use('/jquery', express.static(path.join(__dirname,'./node_modules/jquery/dist/')));
+app.use('/ajax', express.static(path.join(__dirname,'./node_modules/ajax/lib/')));
 
-  app.use('/', routes);
+app.use('/', routes);
 
-  // catch 404 and forward to error handler
-  app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-  });
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
-  // error handlers
+// error handlers
 
-  // development error handler
-  // will print stacktrace
-  if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-      res.status(err.status || 500);
-      res.render('error', {
-        message: err.message,
-        error: err
-      });
-    });
-  }
-
-  // production error handler
-  // no stacktraces leaked to user
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
-      error: {}
+      error: err
     });
   });
-
-  app.listen(3000, function () {
-    console.log('Example app listening on port 8080!')
-  })
-
-  module.exports = app;  
 }
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+if (cluster.isMaster) {  
+  app.listen(3000, function () {
+    console.log('Master listening on port 3000!')
+  })
+} else {
+  app.listen(3001, function () {
+  console.log('Child listening on port 3001')
+})
+}
+
+module.exports = app;  
